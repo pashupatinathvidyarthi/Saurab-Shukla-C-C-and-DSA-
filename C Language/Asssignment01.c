5fe11dd3a65923059959830aa69b8e37fd31d7c5
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-04-05 17:51:10 +0530

    C

---
+++ C Language/Asssignment01.c
@@ -0,0 +1,12 @@
+//Question 01
+#include<Stdio.h>
+#include<stdlib.h>
+int main()
+{
+    system("cls");
+    printf("Hello Students");
+    return 0;
+}
+
+
+//Question 02
--- C Language/tempCodeRunnerFile.c
+++ C Language/tempCodeRunnerFile.c
@@ -1,12 +1,9 @@
-//Write a program to calculate sum of two integers
-#include<stdio.h>
+//Question 01
+#include<Stdio.h>
 #include<stdlib.h>
 int main()
 {
     system("cls");
-    int num1,num2;
-    scanf("%d",&num1);
-    scanf("%d",&num2);
-    printf("Sum is %d",num1+num2);
+    printf("Hello Students");
     return 0;
 }
