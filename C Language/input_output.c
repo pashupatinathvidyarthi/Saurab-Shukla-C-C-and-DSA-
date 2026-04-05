9ebcf564b8aecc417ef446f88590281cd29f861d
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-04-05 11:47:53 +0530

    C

--- C Language/input_output.c
+++ C Language/input_output.c
@@ -37,8 +37,10 @@ printf("Hello");
 */
 
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-
+    system("cls");
+    printf("Welcome");
     return 0;
 }
---
+++ C Language/tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    system("cls");
+    printf("Welcome");
+    return 0;
+}
