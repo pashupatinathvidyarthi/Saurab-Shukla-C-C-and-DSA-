afcce7c77164a926012a93e3a04e0437171cb68c
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-04-05 11:41:24 +0530

    C

---
+++ C Language/input_output.c
@@ -0,0 +1,14 @@
+/*
+
+## 📘 About C Language
+
+- ✅ C is a **block structured programming language**
+- ✅ A C program can have **any number of blocks**
+- ✅ Usually, **outermost blocks are functions**
+- ✅ A function has a **name for identification**
+- ✅ Even the smallest C program has **one function**
+- ✅ All function names must be **unique**
+- ✅ If there is only one function, its name must be `main()`
+- ✅ If there are multiple functions, **one must be `main()`**
+
+*/
