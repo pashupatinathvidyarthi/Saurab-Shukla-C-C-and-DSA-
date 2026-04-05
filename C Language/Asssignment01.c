028e7be34998f38169ce5b376f084e119a17da82
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-04-05 18:01:21 +0530

    C

--- C Language/Asssignment01.c
+++ C Language/Asssignment01.c
@@ -65,3 +65,14 @@ int main()
 
 
 //Question 07
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    system("cls");
+    int num1,num2;
+    printf("Enter two number to get there sum\n");
+    scanf("%d%d",&num1,num2);
+    printf("Sum of %d and %d is %d",num1,num2,num1+num2);
+    return 0;
+}
--- C Language/tempCodeRunnerFile.c
+++ C Language/tempCodeRunnerFile.c
@@ -1,9 +1,12 @@
-//Question 06
+//Question 07
 #include<stdio.h>
-#include<Stdlib.h>
+#include<stdlib.h>
 int main()
 {
     system("cls");
-    printf("\"Teacher\'s Day\"");
+    int num1,num2;
+    printf("Enter two number to get there sum\n");
+    scanf("%d%d",&num1,num2);
+    printf("Sum of %d and %d is %d",num1,num2,num1+num2);
     return 0;
 }
