180e2c27f59f9d02bbb8f3d818a92b01dc9707df
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-04-05 12:23:31 +0530

    C

--- C Language/input_output.c
+++ C Language/input_output.c
@@ -176,3 +176,14 @@ scanf("%d", &a);
 
 
 //Write a program to calculate sum of two integers
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    system("cls");
+    int num1,num2;
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+    printf("Sum is %d",num1+num2);
+    return 0;
+}
--- C Language/tempCodeRunnerFile.c
+++ C Language/tempCodeRunnerFile.c
@@ -1,16 +1,12 @@
+//Write a program to calculate sum of two integers
 #include<stdio.h>
 #include<stdlib.h>
 int main()
 {
     system("cls");
-    int num1=5;
-    int num2=6;
-    printf("%d ",num1);
-    printf("%d\n",num2);
-    printf("a = %d and b= %d\n",num1,num2);
-    printf("a = %d & b = %d\n",num2,num1);
-    printf("Value of a is %d and value of b is %d\n",num1,num2);
-    printf("Sum of %d andd %d is %d\n",num1,num2,num1+num2);
-    printf("%d + %d = %d",num1,num2,num1+num2);
+    int num1,num2;
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+    printf("Sum is %d",num1+num2);
     return 0;
 }
