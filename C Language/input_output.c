e90851e121ebd1cf06469b879ae32892d76e1a67
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-04-05 12:17:48 +0530

    C

--- C Language/input_output.c
+++ C Language/input_output.c
@@ -139,3 +139,20 @@ int a = 5, b = 6;
 
 */
 
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    system("cls");
+    int num1=5;
+    int num2=6;
+    printf("%d ",num1);
+    printf("%d\n",num2);
+    printf("a = %d and b= %d\n",num1,num2);
+    printf("a = %d & b = %d\n",num2,num1);
+    printf("Value of a is %d and value of b is %d\n",num1,num2);
+    printf("Sum of %d and %d is %d\n",num1,num2,num1+num2);
+    printf("%d + %d = %d",num1,num2,num1+num2);
+    return 0;
+}
--- C Language/tempCodeRunnerFile.c
+++ C Language/tempCodeRunnerFile.c
@@ -3,9 +3,14 @@
 int main()
 {
     system("cls");
-    char a='A';
-    float k=4.5;
-    printf("%c\n",a);
-    printf("%.2f",k);
+    int num1=5;
+    int num2=6;
+    printf("%d ",num1);
+    printf("%d\n",num2);
+    printf("a = %d and b= %d\n",num1,num2);
+    printf("a = %d & b = %d\n",num2,num1);
+    printf("Value of a is %d and value of b is %d\n",num1,num2);
+    printf("Sum of %d andd %d is %d\n",num1,num2,num1+num2);
+    printf("%d + %d = %d",num1,num2,num1+num2);
     return 0;
 }
